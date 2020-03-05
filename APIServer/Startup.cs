cb5764a946b3da5316d9d6647d0ae2e2be950631
Oleg Kirasov <okirasov@gmail.com>
2020-03-05 11:15:32 -0500

    HTML pages

--- APIServer/Startup.cs
+++ APIServer/Startup.cs
@@ -41,6 +41,8 @@ namespace APIServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
